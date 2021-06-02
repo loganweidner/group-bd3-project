7d4da3fba4cd2eb4311b7ef9952aa93af382698e
Logan Weidner <weidner1@uw.edu>
2021-06-02 15:09:21 -0700

    remove commented out code, add comments to ui.R

--- application/ui.r
+++ application/ui.r
@@ -1,13 +1,13 @@
 library(shiny)
 
 shinyUI(
-  ui <- navbarPage('Education and Vaccine Hesitancy',
-           tabPanel('About',
+  ui <- navbarPage('Education and Vaccine Hesitancy', #Make Page tabs
+           tabPanel('About', #first tab, 'About' section
               textOutput('This is where we will describe the project and data.')
            ),
-           tabPanel('State Data'
+           tabPanel('State Data' #second tab, state data represented as a map
            ),
-           tabPanel('Graphing Relationships',
+           tabPanel('Graphing Relationships', #third tab, scatterplot made to show user relationship between vars
               titlePanel("Relationship between hesitancy and pupil spending per capita"),
               sidebarLayout(
                 sidebarPanel(
@@ -18,19 +18,8 @@ shinyUI(
                 )
               )
            ),
-           tabPanel('Findings',
+           tabPanel('Findings', #Final tab, findings data
               textOutput('This is where we will write our conclusion and acknowledgements')
            )
         )
-  # fluidPage(
-  #   titlePanel("Relationship between hesitancy and pupil spending per capita"),
-  #   sidebarLayout(
-  #     sidebarPanel(
-  #       uiOutput('x_axis')
-  #     ),
-  #     mainPanel(
-  #       plotOutput("scatterplot")
-  #     )
-  #   )
-  # )
 )
