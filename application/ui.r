13eb7bb7abc429c59510809e64d677f1fc03fa75
Logan Weidner <weidner1@uw.edu>
2021-06-02 14:56:47 -0700

    add navbar panels to UI

--- application/ui.r
+++ application/ui.r
@@ -1,14 +1,36 @@
 library(shiny)
 
 shinyUI(
-  fluidPage(
-    titlePanel("Relationship between hesitancy and pupil spending per capita"),
-    sidebarLayout(
-      sidebarPanel(
-        uiOutput('x_axis')
-      ),
-      mainPanel(
-        plotOutput("scatterplot")
-      )
-    )
-  ))
+  ui <- navbarPage('Education and Vaccine Hesitancy',
+           tabPanel('About',
+              textOutput('This is where we will describe the project and data.')
+           ),
+           tabPanel('State Data'
+           ),
+           tabPanel('Graphing Relationships',
+              titlePanel("Relationship between hesitancy and pupil spending per capita"),
+              sidebarLayout(
+                sidebarPanel(
+                  uiOutput('x_axis')
+                ),
+                mainPanel(
+                  plotOutput("scatterplot")
+                )
+              )
+           ),
+           tabPanel('Findings',
+              textOutput('This is where we will write our conclusion and acknowledgements')
+           )
+        )
+  # fluidPage(
+  #   titlePanel("Relationship between hesitancy and pupil spending per capita"),
+  #   sidebarLayout(
+  #     sidebarPanel(
+  #       uiOutput('x_axis')
+  #     ),
+  #     mainPanel(
+  #       plotOutput("scatterplot")
+  #     )
+  #   )
+  # )
+)
