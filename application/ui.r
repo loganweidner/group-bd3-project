095656fb5d1dd69f6084d296560a77e93c4914a6
Logan Weidner <weidner1@uw.edu>
2021-06-02 21:48:44 -0700

    restore ui

--- application/ui.r
+++ application/ui.r
@@ -1,18 +1,6 @@
 library(shiny)
-library(plotly)
+
 shinyUI(
-  fluidPage(
-    titlePanel("Relationship between hesitancy and pupil spending per capita"),
-    sidebarLayout(
-      sidebarPanel(
-        uiOutput('x_axis')
-      ),
-      mainPanel(
-        plotOutput("scatterplot"),
-        plotlyOutput("mapPlot")
-      )
-    )
-  ))
   ui <- navbarPage('Education and Vaccine Hesitancy', #Make Page tabs
                    tabPanel('About', #first tab, 'About' section
                             textOutput('This is where we will describe the project and data.')
@@ -23,18 +11,15 @@ shinyUI(
                             titlePanel("Relationship between hesitancy and pupil spending per capita"),
                             sidebarLayout(
                               sidebarPanel(
-                                uiOutput('x_axis'),
-                                uiOutput('y_axis')
+                                uiOutput('x_axis')
                               ),
                               mainPanel(
                                 plotOutput("scatterplot")
                               )
                             )
                    ),
-                   tabPanel('Table', #data table of data
-                            dataTableOutput('table')
-                   ),
                    tabPanel('Findings', #Final tab, findings data
                             textOutput('This is where we will write our conclusion and acknowledgements')
                    )
   )
+)
